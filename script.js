568e71fa547a0156840be44d325ba51d0b4ce6d7
riddsep <ridoseptiawan00@gmail.com>
2024-11-03 22:08:26 +0700

    minor changes

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ document.querySelector(".check").addEventListener("click", () => {
 document.querySelector(".again").addEventListener("click", () => {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
-  displayMessage("Start guessing...");
+  displayMessage("Start guessing... 🤔");
   displayScore(score);
   document.querySelector(".number").textContent = "?";
   document.querySelector("body").style.backgroundColor = "#222";
